03b55c474ca3c8ee1f22118a75ab2600aaf9cebd
lhx <2094996209@qq.com>
2025-12-03 09:37:11 +0800

    添加用户姓名输入交互功能

--- cpp-project/src/main.cpp
+++ cpp-project/src/main.cpp
@@ -2,7 +2,15 @@
 #include "../include/utils.h"
 
 int main() {
-    std::cout << "Hello from learngit repository!" << std::endl;
-    printMessage("This project is in my learngit repo");
+    std::cout << "=== C++ Project Demo ===" << std::endl;
+    printMessage("Welcome to C++ Project!");
+    
+    std::cout << "\n--- Basic Interaction ---" << std::endl;
+    std::cout << "Please enter your name: ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!" << std::endl;
+    
+    return 0;
     return 0;
 }
